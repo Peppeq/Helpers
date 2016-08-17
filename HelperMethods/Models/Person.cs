13de2e6714bb2c2009bd20d817b633e7163f8f71
Peppeq <peter.hladky@hpe.com>
2016-08-17 17:14:50 +0200

    page 632

--- HelperMethods/Models/Person.cs
+++ HelperMethods/Models/Person.cs
@@ -1,15 +1,31 @@
 ﻿using System;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace HelperMethods.Models
 {
+    [DisplayName("New Person")]
     public class Person
     {
+        [HiddenInput(DisplayValue=false)]
         public int PersonId { get; set; }
+
+        [Display(Name = "First")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last")]
         public string LastName { get; set; }
+
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
+
         public Address HomeAddress { get; set; }
+
+        [Display(Name = "Approved")]
         public bool IsApproved { get; set; }
+        
         public Role Role { get; set; }
     }
     public class Address
